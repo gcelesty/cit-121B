3476ebb93a6999bd45632521f556e075f4128169
gcelesty <97373234+gcelesty@users.noreply.github.com>
2023-12-16 14:23:58 -0600

    Add files via upload

---
+++ sandbox/debugging.js
@@ -0,0 +1,16 @@
+const PI = 3.14;
+let area = 0;
+function circleArea(radius) {
+  const area = radius * PI;
+  return area;
+}
+area = circleArea(3);
+console.log("Area1: ", area);
+area = circleArea(4);
+console.log("Area1: ", area);
+
+// area = radius * radius * PI;
+// console.log("Area1: ", area);
+// radius = 4;
+// area = radius * radius * PI;
+// console.log("Area2: ", area);
